1b3e9b6cc33a9c7addf8c241636fbfc77aac6691
varunganesh91 <operationgenesis@gmail.com>
2016-01-19 00:15:45 +0800

    Adding tests for GET product by ID, category

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ var assert = require('assert');
 var superagent = require('superagent');
 var wagner = require('wagner-core');
 
+var OBJECT_ID = '000000000000000000000001';
+
 describe('MEANBazaar Test', function(){
 	var app;
 	var server;
@@ -22,22 +24,16 @@ describe('MEANBazaar Test', function(){
 	describe('Category API', function(){
 		it('Inserts and gets a category by id', function(done){
 			var category = {
-				_id : 'Phone',
+				_id : 'Phones',
 				parent: 'Electronics',
 			};
 
 			Category.create(category, function(err){
 				assert.ifError(err);
-				Category.count({}, function(err, count){
-					assert.ifError(err);
-					assert.equal(count, 1);
-				});
-
-				superagent.get('http://localhost:3000/api/v1/category/id/Phone', function(error, res) {
+				superagent.get('http://localhost:3000/api/v1/category/id/Phones', function(error, res) {
 					assert.ifError(error);
 					assert.equal(res.status, 200);
 					var response = JSON.parse(res.text);
-					console.log(response);
 					assert.equal(response.category.parent, "Electronics");
 					done();
 			    });	
@@ -46,7 +42,7 @@ describe('MEANBazaar Test', function(){
 
 	    it('Gets categories by Parent id', function(done){
 			var category = {
-				_id : 'Laptop',
+				_id : 'Laptops',
 				parent: 'Electronics',
 			};
 
@@ -62,31 +58,109 @@ describe('MEANBazaar Test', function(){
 			    });	
 			});
 	    });
+
+	    it('Gets a product by object id', function(done){
+	    	superagent.get('http://localhost:3000/api/v1/product/id/'+ OBJECT_ID, function(error, res) {
+					assert.ifError(error);
+					assert.equal(res.status, 200);
+					var response;
+					assert.doesNotThrow(function(){
+						response = JSON.parse(res.text);
+					});
+					assert.equal(response.product.name, 'Nexus 5x');
+					done();
+			});
+	    });
+
+		it('Gets products by category sorted ascending by price', function(done){
+	    	superagent.get('http://localhost:3000/api/v1/product/category/Electronics?price=1', function(error, res) {
+					assert.ifError(error);
+					assert.equal(res.status, 200);
+					var response;
+					assert.doesNotThrow(function(){
+						response = JSON.parse(res.text);
+					});
+					assert.equal(response.products.length, 2);
+					assert.equal(response.products[0].name, 'Nexus 5x');
+					assert.equal(response.products[1].internal.approximatePriceUSD, 2000);
+					done();
+			});	
+	    })
 	});
 
-  //Clear db before tests
-  before(function(done){
+
+  before(function(){
   	app = express();
   	//Bootstrap server
     models = require('./models')(wagner);
 
     //Get models for tests 
-    var deps = wagner.invoke(function(Category){
+    var deps = wagner.invoke(function(Category, Product){
     	return {
-    		Category: Category
+    		Category : Category,
+    		Product : Product
     	};
     })
 
     Category = deps.Category;
-    Category.remove(function(err){
-      if(err){
-        done(err);
-      }
-      done();
-    });
+    Product = deps.Product;
 
     app.use('/api/v1', require('./api.js')(wagner));
   	server = app.listen(3000);
+  });
+
+  //Clear db before tests
+  before(function(done){
+    Category.remove({}, function(error) {
+      assert.ifError(error);
+      Product.remove({}, function(error) {
+        assert.ifError(error);
+        done();
+      });
+    });
+  });
+
+  before(function(done){
+  	var categories = [
+      { _id: 'Electronics' },
+      { _id: 'Bacon' }
+    ];
+
+    var products = [
+      {
+      	_id: OBJECT_ID,
+        name: 'Nexus 5x',
+        category: { _id: 'Phones', ancestors: ['Electronics', 'Phones'] },
+        price: {
+          amount: 400,
+          currency: 'USD'
+        }
+      },
+      {
+        name: 'Asus Zenbook Prime',
+        category: { _id: 'Laptops', ancestors: ['Electronics', 'Laptops'] },
+        price: {
+          amount: 2000,
+          currency: 'USD'
+        }
+      },
+      {
+        name: 'Flying Pigs Farm Pasture Raised Pork Bacon',
+        category: { _id: 'Bacon', ancestors: ['Bacon'] },
+        price: {
+          amount: 20,
+          currency: 'USD'
+        }
+      }
+    ];
+
+    Category.create(categories, function(error) {
+      assert.ifError(error);
+      Product.create(products, function(error) {
+        assert.ifError(error);
+        done();
+      });
+    });
   });
 
   after(function(done) {
