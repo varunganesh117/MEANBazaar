ecfb13c1dc22d99340548a83a87f8ef770912203
varunganesh91 <operationgenesis@gmail.com>
2016-01-30 17:49:28 +0800

    Setting up browserify for angular/index.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var browserify = require('gulp-browserify');
+
+gulp.task('browserify', function() {
+  return gulp.
+    src('./angular/index.js').
+    pipe(browserify()).
+    pipe(gulp.dest('./angular/bin'));
+});
 
 gulp.task('test', function() {
   var error = false;
@@ -17,6 +25,7 @@ gulp.task('test', function() {
     });
 });
 
-gulp.task('watch', ['test'], function() {
+gulp.task('watch', function() {
   gulp.watch(['./*.js'], ['test']);
+  gulp.watch(['./angular/*.js'], ['browserify']);
 });
