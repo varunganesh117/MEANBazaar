dab40e6bce957fdb4753500c26eca8e9c4d7a245
varunganesh91 <operationgenesis@gmail.com>
2016-01-31 17:27:53 +0800

    Adding ECI test and ngTest to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var browserify = require('gulp-browserify');
+var Server = require('karma').Server;
+
+
+gulp.task('ngtest', function (done) {
+  new Server({
+    configFile: __dirname + '/angular/karma.local.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
 gulp.task('browserify', function() {
   return gulp.
@@ -29,3 +38,5 @@ gulp.task('watch', function() {
   gulp.watch(['./*.js'], ['test']);
   gulp.watch(['./angular/*.js'], ['browserify']);
 });
+
+gulp.task('ECI', ['test', 'browserify', 'ngtest']);
